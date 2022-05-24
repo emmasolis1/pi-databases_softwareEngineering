a5c8562e6bd0ae2f200ff1da21405d8e52772649
Gabriel Zúñiga Orozco <GABRIEL.ZUNIGAOROZCO@ucr.ac.cr>
2022-05-24 13:33:44 -0600

    Working backend for ObligatoryDeductions and creation of Variables file to call Api

--- Planilla/planilla-backend_asp.net/Models/ObligatoryDeductionsModel.cs
+++ Planilla/planilla-backend_asp.net/Models/ObligatoryDeductionsModel.cs
@@ -1,13 +1,7 @@
 ﻿namespace planilla_backend_asp.net.Models
 {
-    public  class ObligatoryDeductionsModel
-    {
-        public ObligatoryDeductionsModel()
-        {
-            nombre = "";
-            porcentaje = 0;
-        }
-   
+    public class ObligatoryDeductionsModel
+    {   
         public string nombre { get; set; }
 
         public double porcentaje { get; set; }
