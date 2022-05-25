347674b152b01f061f86978d88c1c53b2e9213c9
Emmanuel Solis <emmanuel.solisp@icloud.com>
2022-05-25 12:50:18 -0600

    new: successfully added connection between frontend to backend with NO ISSUES

--- Planilla/planilla-frontend_react/src/Employees.js
+++ Planilla/planilla-frontend_react/src/Employees.js
@@ -7,43 +7,7 @@ import EmployeeMainInfo from './components/EmployeeMainInfo';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-const Employees = () => {
-  return (
-    <div className="App">
-
-      <SideNavigationBar />
-      <Header />
-
-      <div className="App-header">
-
-        <div style={titleStyle}>
-          <h2 style={{ paddingRight: '20pt' }}>Empleados</h2>
-          <Button color="primary" outline>
-            Agregar un nuevo empleado
-          </Button>
-        </div>
-
-        <EmployeeMainInfo name="Emmanuel D. Solis" isActive="true" jobPosition="Scrum Master" employeeType="Tiempo completo" paymentType="Mensual" netSalary="$15000" sumDeductions="$2000" realSalary="$13000" extraBenefits="$100" />
-        <EmployeeMainInfo name="Jan Murillo" isActive="true" jobPosition="Developer" employeeType="Medio tiempo" paymentType="Semanal" netSalary="$7500" sumDeductions="$1000" realSalary="$6000" extraBenefits="$50" />
-
-      </div>
-
-      <Footer />
-      
-    </div>
-  );
-};
-
-const titleStyle = {
-  display: 'inline-flex',
-  flexDirection: 'row',
-  flexWrap: 'no-wrap',
-  alignItems: 'center',
-  paddingTop: '10pt', 
-  paddingLeft: '60pt'
-}
-
-/*export default Employees;*/
+// Original component form: <EmployeeMainInfo name="Emmanuel D. Solis" isActive="true" jobPosition="Scrum Master" employeeType="Tiempo completo" paymentType="Mensual" netSalary="$15000" sumDeductions="$2000" realSalary="$13000" extraBenefits="$100" />
 
 
 export default class PersonList extends React.Component {
@@ -61,15 +25,49 @@ export default class PersonList extends React.Component {
 
   render() {
     return (
-      <ul>
-        {
-          this.state.persons
-            .map(person =>
-              <li key={person.nombre}>{person.apellido1}</li>
-            )
+      <div className="App">
+
+      <SideNavigationBar />
+      <Header />
+
+      <div className="App-header">
+
+        <div style={titleStyle}>
+          <h2 style={{ paddingRight: '20pt' }}>Empleados</h2>
+          <Button color="primary" outline>
+            Agregar un nuevo empleado
+          </Button>
+        </div>
+          {
+            this.state.persons.map(person =>
+              <EmployeeMainInfo
+                name={person.nombre + " " + person.apellido1 + " " + person.apellido2}
+                isActive="true"
+                jobPosition="Scrum Master"
+                employeeType="Tiempo completo"
+                paymentType="Mensual"
+                netSalary="$15000"
+                sumDeductions="$2000"
+                realSalary="$13000"
+                extraBenefits="$100" />
+          )
         }
-      </ul>
+
+      </div>
+
+      <Footer />
+      
+    </div>
     )
   }
+}
+
+const titleStyle = {
+  display: 'inline-flex',
+  flexDirection: 'row',
+  flexWrap: 'no-wrap',
+  alignItems: 'center',
+  paddingTop: '10pt',
+  paddingLeft: '60pt'
 }
 
