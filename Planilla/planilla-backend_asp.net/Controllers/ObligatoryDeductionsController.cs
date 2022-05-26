fd51d99dc7b0961a18f54445783d39e54f031a67
Emmanuel Solis <emmanuel.solisp@icloud.com>
2022-05-26 14:38:06 -0600

    modularized API code to get mandatory deductions

--- Planilla/planilla-backend_asp.net/Controllers/ObligatoryDeductionsController.cs
+++ Planilla/planilla-backend_asp.net/Controllers/ObligatoryDeductionsController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using planilla_backend_asp.net.Models;
-using System.Data;
-using System.Data.SqlClient;
+using planilla_backend_asp.net.Handlers;
 
 namespace planilla_backend_asp.net.Controllers
 {
@@ -12,42 +10,9 @@ namespace planilla_backend_asp.net.Controllers
         [HttpGet]
         public ActionResult GetObligatoryDeductions()
         {
-            var builder = WebApplication.CreateBuilder();
-            rutaConexion = builder.Configuration.GetConnectionString("EmpleadorContext");
-            conexion = new SqlConnection(rutaConexion);
-            var data = GetObligatoryDeductionsData();
-            return Ok(data);
-        }
-
-        private static SqlConnection conexion;
-        private string rutaConexion;
-
-        private DataTable CreateTableConsult(string consult)
-        {
-            SqlCommand comandoParaConsulta = new SqlCommand(consult, conexion);
-            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
-            DataTable consultaFormatoTabla = new DataTable();
-            conexion.Open();
-            adaptadorParaTabla.Fill(consultaFormatoTabla);
-            conexion.Close();
-            return consultaFormatoTabla;
-        }
-
-        private List<ObligatoryDeductionsModel> GetObligatoryDeductionsData()
-        {
-            List<ObligatoryDeductionsModel> obligatoryDeductions = new List<ObligatoryDeductionsModel>();
-            string consult = "SELECT * FROM DeduccionesObligatorias";
-            DataTable tablaResultado = CreateTableConsult(consult);
-            foreach (DataRow columna in tablaResultado.Rows)
-            {
-                obligatoryDeductions.Add(new ObligatoryDeductionsModel
-                {
-                    nombre = Convert.ToString(columna["Nombre"]),
-                    porcentaje = Convert.ToDouble(columna["Porcentaje"])
-                });
-            }
-
-            return obligatoryDeductions;
+          var handler = new MandatoryDeductionsHandler();
+          var data = handler.getMandatoryDeductions();
+          return Ok(data);
         }
     }
 }
