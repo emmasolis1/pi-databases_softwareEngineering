f4f1b94d6f3ec9ff789aeacb35858c4ab814c171
Emmanuel Solis <emmanuel.solisp@icloud.com>
2022-05-26 14:24:14 -0600

    modularized API code to get benefits

--- Planilla/planilla-backend_asp.net/Controllers/BenefitsController.cs
+++ Planilla/planilla-backend_asp.net/Controllers/BenefitsController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using planilla_backend_asp.net.Models;
-using System.Data;
-using System.Data.SqlClient;
+using planilla_backend_asp.net.Handlers;
 
 namespace planilla_backend_asp.net.Controllers
 {
@@ -12,43 +10,9 @@ namespace planilla_backend_asp.net.Controllers
         [HttpGet]
         public ActionResult GetBenefits()
         {
-            var builder = WebApplication.CreateBuilder();
-            rutaConexion = builder.Configuration.GetConnectionString("EmpleadorContext");
-            conexion = new SqlConnection(rutaConexion);
-            var data = GetBenefitsData();
+            var handler = new BenefitsHandler();
+            var data = handler.GetBenefitsData();
             return Ok(data);
-        }
-
-        private static SqlConnection conexion;
-        private string rutaConexion;
-
-        private DataTable CreateTableConsult(string consult)
-        {
-            SqlCommand comandoParaConsulta = new SqlCommand(consult, conexion);
-            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
-            DataTable consultaFormatoTabla = new DataTable();
-            conexion.Open();
-            adaptadorParaTabla.Fill(consultaFormatoTabla);
-            conexion.Close();
-            return consultaFormatoTabla;
-        }
-
-        private List<BenefitsModel> GetBenefitsData()
-        {
-            List<BenefitsModel> benefits = new List<BenefitsModel>();
-            string consult = "SELECT * FROM Beneficios";
-            DataTable tablaResultado = CreateTableConsult(consult);
-            foreach (DataRow columna in tablaResultado.Rows)
-            {
-                benefits.Add(new BenefitsModel
-                {
-                    nombreBeneficio = Convert.ToString(columna["nombreBeneficio"]),
-                    cedulaEmpleador = Convert.ToString(columna["cedulaEmpleador"]),
-                    nombreProyecto = Convert.ToString(columna["nombreProyecto"]),
-                });
-            }
-
-            return benefits;
         }
     }
 }
---
+++ Planilla/planilla-backend_asp.net/Handlers/BenefitsHandler.cs
@@ -0,0 +1,47 @@
+﻿using planilla_backend_asp.net.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace planilla_backend_asp.net.Handlers
+{
+  public class BenefitsHandler
+  {
+    private static SqlConnection conexion;
+    private string rutaConexion;
+    public BenefitsHandler() {
+      var builder = WebApplication.CreateBuilder();
+      rutaConexion = builder.Configuration.GetConnectionString("EmpleadorContext");
+      conexion = new SqlConnection(rutaConexion);
+    }
+
+    private DataTable CreateTableConsult(string consult)
+    {
+      SqlCommand comandoParaConsulta = new SqlCommand(consult, conexion);
+      SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
+      DataTable consultaFormatoTabla = new DataTable();
+      conexion.Open();
+      adaptadorParaTabla.Fill(consultaFormatoTabla);
+      conexion.Close();
+      
+      return consultaFormatoTabla;
+    }
+
+    public List<BenefitsModel> GetBenefitsData()
+    {
+      List<BenefitsModel> benefits = new List<BenefitsModel>();
+      string consult = "SELECT * FROM Beneficios";
+      DataTable tablaResultado = CreateTableConsult(consult);
+      foreach (DataRow columna in tablaResultado.Rows)
+      {
+        benefits.Add(new BenefitsModel
+        {
+          nombreBeneficio = Convert.ToString(columna["nombreBeneficio"]),
+          cedulaEmpleador = Convert.ToString(columna["cedulaEmpleador"]),
+          nombreProyecto = Convert.ToString(columna["nombreProyecto"]),
+        });
+      }
+
+      return benefits;
+    }
+  }
+}
