d51dc9d7d117c1b2d7af02980e22e88562be5826
Emmanuel Solis <emmanuel.solisp@icloud.com>
2022-05-26 14:32:47 -0600

    modularized API code to get projects

--- Planilla/planilla-backend_asp.net/Controllers/ProjectController.cs
+++ Planilla/planilla-backend_asp.net/Controllers/ProjectController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using planilla_backend_asp.net.Models;
-using System.Data;
-using System.Data.SqlClient;
+using planilla_backend_asp.net.Handlers;
 
 namespace planilla_backend_asp.net.Controllers
 {
@@ -12,44 +10,9 @@ namespace planilla_backend_asp.net.Controllers
         [HttpGet]
         public ActionResult GetProjects()
         {
-            var builder = WebApplication.CreateBuilder();
-            rutaConexion = builder.Configuration.GetConnectionString("EmpleadorContext");
-            conexion = new SqlConnection(rutaConexion);
-            var data = GetProyectsData();
-            return Ok(data);
-        }
-
-        private static SqlConnection conexion;
-        private string rutaConexion;
-
-        private DataTable CreateTableConsult(string consult)
-        {
-            SqlCommand comandoParaConsulta = new SqlCommand(consult, conexion);
-            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
-            DataTable consultaFormatoTabla = new DataTable();
-            conexion.Open();
-            adaptadorParaTabla.Fill(consultaFormatoTabla);
-            conexion.Close();
-            return consultaFormatoTabla;
-        }
-
-        private List<ProjectModel> GetProyectsData()
-        {
-            List<ProjectModel> projects = new List<ProjectModel>();
-            string consult = "SELECT * FROM Proyecto";
-            DataTable tablaResultado = CreateTableConsult(consult);
-            foreach (DataRow columna in tablaResultado.Rows)
-            {
-                projects.Add(new ProjectModel
-                {
-                    nombre = Convert.ToString(columna["nombre"]),
-                    cedulaUsuario = Convert.ToString(columna["cedulaEmpleador"]),
-                    presupuesto = Convert.ToInt32(columna["presupuesto"]),
-                    modalidadPago = Convert.ToString(columna["modalidadPago"])
-                });
-            }
-
-            return projects;
+          var handler = new ProjectHandler();
+          var data = handler.GetProyectsData();
+          return Ok(data);
         }
     }
 }
---
+++ Planilla/planilla-backend_asp.net/Handlers/ProjectHandler.cs
@@ -0,0 +1,48 @@
+﻿using planilla_backend_asp.net.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace planilla_backend_asp.net.Handlers
+{
+  public class ProjectHandler
+  {
+    private static SqlConnection conexion;
+    private string rutaConexion;
+    public ProjectHandler()
+    {
+      var builder = WebApplication.CreateBuilder();
+      rutaConexion = builder.Configuration.GetConnectionString("EmpleadorContext");
+      conexion = new SqlConnection(rutaConexion);
+    }
+
+    private DataTable CreateTableConsult(string consult)
+    {
+      SqlCommand comandoParaConsulta = new SqlCommand(consult, conexion);
+      SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
+      DataTable consultaFormatoTabla = new DataTable();
+      conexion.Open();
+      adaptadorParaTabla.Fill(consultaFormatoTabla);
+      conexion.Close();
+      return consultaFormatoTabla;
+    }
+
+    public List<ProjectModel> GetProyectsData()
+    {
+      List<ProjectModel> projects = new List<ProjectModel>();
+      string consult = "SELECT * FROM Proyecto";
+      DataTable tablaResultado = CreateTableConsult(consult);
+      foreach (DataRow columna in tablaResultado.Rows)
+      {
+        projects.Add(new ProjectModel
+        {
+          nombre = Convert.ToString(columna["nombre"]),
+          cedulaUsuario = Convert.ToString(columna["cedulaEmpleador"]),
+          presupuesto = Convert.ToInt32(columna["presupuesto"]),
+          modalidadPago = Convert.ToString(columna["modalidadPago"])
+        });
+      }
+
+      return projects;
+    }
+  }
+}
