0807f1f61da1045b89fefdf531956b6201b5f13c
Kevin <kevinarguedas1820@gmail.com>
2022-06-11 22:44:32 -0600

    Added VoluntaryDeductions controller, handler and model. Also made index methos to consult API (not finished)

---
+++ Planilla/planilla-backend_asp.net/Handlers/VoluntaryDeductionHandler.cs
@@ -0,0 +1,49 @@
+﻿using planilla_backend_asp.net.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace planilla_backend_asp.net.Handlers
+{
+    public class VoluntaryDeductionsHandler
+    {
+        private static SqlConnection conexion;
+        private string rutaConexion;
+        public VoluntaryDeductionsHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            rutaConexion = builder.Configuration.GetConnectionString("EmpleadorContext");
+            conexion = new SqlConnection(rutaConexion);
+        }
+
+        public List<VoluntaryDeductionsModel> GetDeductions()
+        {
+            List<VoluntaryDeductionsModel> deductions = new List<VoluntaryDeductionsModel>();
+            string consulta = "SELECT * FROM VoluntaryDeductions";
+            DataTable tablaResultado = CreateTableConsult(consulta);
+            foreach (DataRow columna in tablaResultado.Rows)
+            {
+                deductions.Add(
+                  new VoluntaryDeductionsModel
+                  {
+                      VoluntaryDeductionName = Convert.ToString(columna["Nombre"]),
+                      ProjectName = Convert.ToString("NombreProyecto"),
+                      EmployerID = Convert.ToString(columna["CedulaEmpleador"]),
+                      Description = Convert.ToString(columna["Descripcion"])
+                  });
+            }
+            return deductions;
+        }
+
+        private DataTable CreateTableConsult(string consult)
+        {
+            SqlCommand comandoParaConsulta = new SqlCommand(consult, conexion);
+            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new DataTable();
+            conexion.Open();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            conexion.Close();
+            return consultaFormatoTabla;
+        }
+    }
+}
+
--- Planilla/planilla-frontend_react/src/pages/index.js
+++ Planilla/planilla-frontend_react/src/pages/index.js
@@ -9,103 +9,126 @@ import { TotalCustomers } from '../components/dashboard/total-customers';
 import { TotalProfit } from '../components/dashboard/total-profit';
 import { TrafficByDevice } from '../components/dashboard/traffic-by-device';
 import { DashboardLayout } from '../components/dashboard-layout';
+import React, { useState, useEffect } from 'react';
 
-const Dashboard = () => (
-  <>
-    <Head>
-      <title>
-        Dashboard | Material Kit
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth={false}>
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <Budget />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TotalCustomers />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TasksProgress />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TotalProfit sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <Sales />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <TrafficByDevice sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <LatestProducts sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <LatestOrders />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  </>
-);
+function useApiInfo() {
+    const [employees, setEmployees] = useState(null);
+    useEffect(() => {
+        const getEmployees = async () => {
+            const response = await fetch('http://localhost:5150/api/employees');
+            const data = await response.json();
+            console.log("Se obtuvo del API: " + data);
+            setEmployees(data);
+            console.log("El valor de employees se cambio a: " + employees);
+        };
+        getEmployees();
+    }, []);
+    console.log("El valor es: " + employees);
+    return (employees);
+}
+
+const Dashboard = () => {
+    let employees = useApiInfo();
+    if (employees) {
+        
+    }
+    return (
+        <>
+            <Head>
+                <title>
+                    Dashboard | Material Kit
+                </title>
+            </Head>
+            <Box
+                component="main"
+                sx={{
+                    flexGrow: 1,
+                    py: 8
+                }}
+            >
+                <Container maxWidth={false}>
+                    <Grid
+                        container
+                        spacing={3}
+                    >
+                        <Grid
+                            item
+                            lg={3}
+                            sm={6}
+                            xl={3}
+                            xs={12}
+                        >
+                            <Budget />
+                        </Grid>
+                        <Grid
+                            item
+                            xl={3}
+                            lg={3}
+                            sm={6}
+                            xs={12}
+                        >
+                            <TotalCustomers />
+                        </Grid>
+                        <Grid
+                            item
+                            xl={3}
+                            lg={3}
+                            sm={6}
+                            xs={12}
+                        >
+                            <TasksProgress />
+                        </Grid>
+                        <Grid
+                            item
+                            xl={3}
+                            lg={3}
+                            sm={6}
+                            xs={12}
+                        >
+                            <TotalProfit sx={{ height: '100%' }} />
+                        </Grid>
+                        <Grid
+                            item
+                            lg={8}
+                            md={12}
+                            xl={9}
+                            xs={12}
+                        >
+                            <Sales />
+                        </Grid>
+                        <Grid
+                            item
+                            lg={4}
+                            md={6}
+                            xl={3}
+                            xs={12}
+                        >
+                            <TrafficByDevice sx={{ height: '100%' }} />
+                        </Grid>
+                        <Grid
+                            item
+                            lg={4}
+                            md={6}
+                            xl={3}
+                            xs={12}
+                        >
+                            <LatestProducts sx={{ height: '100%' }} />
+                        </Grid>
+                        <Grid
+                            item
+                            lg={8}
+                            md={12}
+                            xl={9}
+                            xs={12}
+                        >
+                            <LatestOrders />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </>
+    );
+}
 
 Dashboard.getLayout = (page) => (
   <DashboardLayout>
