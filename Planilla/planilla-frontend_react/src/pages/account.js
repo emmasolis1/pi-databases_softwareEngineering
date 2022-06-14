e15dbcc5455bb624d45fc4622d6dee360aaaf584
Emmanuel S <emmanuel.solisp@icloud.com>
2022-06-13 20:44:35 -0600

    refactor file to adapt to our backend

--- Planilla/planilla-frontend_react/src/pages/account.js
+++ Planilla/planilla-frontend_react/src/pages/account.js
@@ -1,55 +1,77 @@
 import Head from 'next/head';
+import React from 'react';
 import { Box, Container, Grid, Typography } from '@mui/material';
 import { AccountProfile } from '../components/account/account-profile';
 import { AccountProfileDetails } from '../components/account/account-profile-details';
 import { DashboardLayout } from '../components/dashboard-layout';
+import axios from 'axios';
 
-const Account = () => (
-  <>
-    <Head>
-      <title>
-        Account | Material Kit
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth="lg">
-        <Typography
-          sx={{ mb: 3 }}
-          variant="h4"
+class Account extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: [],
+      APIUrl: 'https://localhost:7150/api/editProfile',
+    };
+  }
+
+  componentDidMount() {
+    //var bodyData = {id: localStorage.getItem('id')};
+    var bodyData = {id: "1234567899"};
+    axios.post(this.state.APIUrl, bodyData).then(response => {
+      this.setState({ user: (response.data)[0] });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Head>
+          <title>
+            Account | Material Kit
+          </title>
+        </Head>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            py: 8
+          }}
         >
-          Account
-        </Typography>
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xs={12}
-          >
-            <AccountProfile />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={6}
-            xs={12}
-          >
-            <AccountProfileDetails />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  </>
-);
+          <Container maxWidth="lg">
+            <Typography
+              sx={{ mb: 3 }}
+              variant="h4"
+            >
+              Account
+            </Typography>
+            <Grid
+              container
+              spacing={3}
+            >
+              <Grid
+                item
+                lg={4}
+                md={6}
+                xs={12}
+              >
+                <AccountProfile user={this.state.user}/>
+              </Grid>
+              <Grid
+                item
+                lg={8}
+                md={6}
+                xs={12}
+              >
+                <AccountProfileDetails user={this.state.user}/>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      </>
+    );
+  }
+}
 
 Account.getLayout = (page) => (
   <DashboardLayout>
