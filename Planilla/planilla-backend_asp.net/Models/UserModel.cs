b03173f91a967737f7a923f2dea07e294a1287c1
Emmanuel S <emmanuel.solisp@icloud.com>
2022-06-15 11:54:08 -0600

    modified struct to allow password

--- Planilla/planilla-backend_asp.net/Models/UserModel.cs
+++ Planilla/planilla-backend_asp.net/Models/UserModel.cs
@@ -31,6 +31,7 @@
     public class UserEmployeeInfoToModify
     {
         public string Identification { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
         public string Country { get; set; }
         public string State { get; set; }
