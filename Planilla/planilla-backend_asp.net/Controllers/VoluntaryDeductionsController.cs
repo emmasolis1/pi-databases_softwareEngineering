92983359761ecd009e10b6af804c2f765d4294d4
Diego Chinchilla <diego.chinchilla98@gmail.com>
2022-06-17 14:36:06 -0600

    TB-18 Voluntary Deductions creation backend

---
+++ Planilla/planilla-backend_asp.net/Controllers/VoluntaryDeductionsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using planilla_backend_asp.net.Models;
+using planilla_backend_asp.net.Handlers;
+
+namespace planilla_backend_asp.net.Controllers
+{
+  [Route("api/")]
+  [ApiController]
+  public class VoluntaryDeductionsController : ControllerBase
+  {
+    [HttpPost]
+    [Route("voluntaryDeductions")]
+    public ActionResult CreateVoluntaryDeduction([FromBody] VoluntaryDeductionsModel voluntaryDeductions)
+    {
+      // Create new benefit
+      VoluntaryDeductionsHandler handler = new VoluntaryDeductionsHandler();
+      handler.CreateVoluntaryDeductions(voluntaryDeductions);
+      return Ok();
+    }
+  }
+}
