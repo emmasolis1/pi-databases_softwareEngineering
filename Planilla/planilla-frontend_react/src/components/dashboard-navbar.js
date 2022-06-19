c23e179ee822ab9c2beceeca2a19b929c947d2aa
Emmanuel S <emmanuel.solisp@icloud.com>
2022-06-18 22:52:29 -0600

    added user icon to avatar

--- Planilla/planilla-frontend_react/src/components/dashboard-navbar.js
+++ Planilla/planilla-frontend_react/src/components/dashboard-navbar.js
@@ -81,7 +81,7 @@ export const DashboardNavbar = (props) => {
                   width: 40,
                   ml: 1
                 }}
-                src="/static/images/avatars/avatar_1.png"
+                // src="/static/images/avatars/avatar_1.png"
                 >
                   <UserCircleIcon fontSize="medium" />
                 </Avatar>
