ee3a355f1cbf5ab3199f70bc860d1b971c0652b5
Jan Murillo <janmb191@gmail.com>
2022-06-20 17:58:24 -0600

    U´dated the benefit profile side of the edit view

---
+++ Planilla/planilla-frontend_react/src/components/specificBenefit/benefit-profile.js
@@ -0,0 +1,43 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Divider,
+  Typography
+} from '@mui/material';
+import { useRouter } from 'next/router';
+
+export const BenefitProfile = ({ benefit, ...props }) => {
+  return (
+    <Card {...props}>
+      <CardContent>
+        <Box
+          sx={{
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+        >
+          <Typography
+            color="textPrimary"
+            gutterBottom
+            variant="h5"
+          >
+            {benefit.benefitName}
+          </Typography>
+          <Typography
+            color="textSecondary"
+            variant="body2"
+          >
+            Based on project: {benefit.projectName}
+          </Typography>
+        </Box>
+      </CardContent>
+      <Divider />
+      <CardActions>
+      </CardActions>
+    </Card>
+  );
+};
