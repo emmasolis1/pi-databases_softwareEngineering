a972bd0b79e386bc644f38b776200cd3f77e0896
Emmanuel S <emmanuel.solispomares@ucr.ac.cr>
2022-07-12 22:46:16 -0600

    successfully retrieved dashboard from backend

--- Planilla/planilla-frontend_react/src/pages/dashboard.js
+++ Planilla/planilla-frontend_react/src/pages/dashboard.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import React from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 import { Box, Container, Grid } from '@mui/material';
 import { Budget } from '../components/dashboard/budget';
 import { LatestOrders } from '../components/dashboard/latest-orders';
@@ -10,124 +12,128 @@ import { TotalCustomers } from '../components/dashboard/total-customers';
 import { TotalProfit } from '../components/dashboard/total-profit';
 import { TrafficByDevice } from '../components/dashboard/traffic-by-device';
 import { DashboardLayout } from '../components/dashboard-layout';
+import { URL } from 'src/utils/url';
+import { parseJSON } from 'date-fns';
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoaded: false,
-      user: {},
+      info: [],
+      APIUrl: URL + 'dashboard',
     };
   }
 
   componentDidMount() {
-    this.setState({
-      isLoaded: true,
-      user: {
-        name: 'John Doe',
-        avatar: 'https://.com/images/avatars/avatar_2.png',
-        email: ''
-      },
+    var projects = JSON.parse(sessionStorage.getItem('userProjects'));
+    let requestAPI = this.state.APIUrl + "?employerID=" + sessionStorage.getItem("employerID") + "&projectName=" + projects[0];
+    axios.get(requestAPI).then(response => {
+      this.setState({ isLoaded: true, info: response.data });
     });
   }
 
   render() {
-    return (
-      <>
-        <Head>
-          <title>
-            Dashboard | Ta' Bueno
-          </title>
-        </Head>
-        <Box
-          component="main"
-          sx={{
-            flexGrow: 1,
-            py: 8
-          }}
-        >
-          <Container maxWidth={false}>
-            <Grid
-              container
-              spacing={3}
-            >
+    if (!this.state.isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <>
+          <Head>
+            <title>
+              Dashboard | Ta' Bueno
+            </title>
+          </Head>
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              py: 8
+            }}
+          >
+            <Container maxWidth={false}>
               <Grid
-                item
-                lg={3}
-                sm={6}
-                xl={3}
-                xs={12}
+                container
+                spacing={3}
               >
-                <Budget />
+                <Grid
+                  item
+                  lg={3}
+                  sm={6}
+                  xl={3}
+                  xs={12}
+                >
+                  <Budget />
+                </Grid>
+                <Grid
+                  item
+                  xl={3}
+                  lg={3}
+                  sm={6}
+                  xs={12}
+                >
+                  <TotalCustomers />
+                </Grid>
+                <Grid
+                  item
+                  xl={3}
+                  lg={3}
+                  sm={6}
+                  xs={12}
+                >
+                  <TasksProgress />
+                </Grid>
+                <Grid
+                  item
+                  xl={3}
+                  lg={3}
+                  sm={6}
+                  xs={12}
+                >
+                  <TotalProfit sx={{ height: '100%' }} />
+                </Grid>
+                <Grid
+                  item
+                  lg={8}
+                  md={12}
+                  xl={9}
+                  xs={12}
+                >
+                  <Sales />
+                </Grid>
+                <Grid
+                  item
+                  lg={4}
+                  md={6}
+                  xl={3}
+                  xs={12}
+                >
+                  <TrafficByDevice sx={{ height: '100%' }} />
+                </Grid>
+                <Grid
+                  item
+                  lg={4}
+                  md={6}
+                  xl={3}
+                  xs={12}
+                >
+                  <LatestProducts sx={{ height: '100%' }} />
+                </Grid>
+                <Grid
+                  item
+                  lg={8}
+                  md={12}
+                  xl={9}
+                  xs={12}
+                >
+                  <LatestOrders />
+                </Grid>
               </Grid>
-              <Grid
-                item
-                xl={3}
-                lg={3}
-                sm={6}
-                xs={12}
-              >
-                <TotalCustomers />
-              </Grid>
-              <Grid
-                item
-                xl={3}
-                lg={3}
-                sm={6}
-                xs={12}
-              >
-                <TasksProgress />
-              </Grid>
-              <Grid
-                item
-                xl={3}
-                lg={3}
-                sm={6}
-                xs={12}
-              >
-                <TotalProfit sx={{ height: '100%' }} />
-              </Grid>
-              <Grid
-                item
-                lg={8}
-                md={12}
-                xl={9}
-                xs={12}
-              >
-                <Sales />
-              </Grid>
-              <Grid
-                item
-                lg={4}
-                md={6}
-                xl={3}
-                xs={12}
-              >
-                <TrafficByDevice sx={{ height: '100%' }} />
-              </Grid>
-              <Grid
-                item
-                lg={4}
-                md={6}
-                xl={3}
-                xs={12}
-              >
-                <LatestProducts sx={{ height: '100%' }} />
-              </Grid>
-              <Grid
-                item
-                lg={8}
-                md={12}
-                xl={9}
-                xs={12}
-              >
-                <LatestOrders />
-              </Grid>
-            </Grid>
-          </Container>
-        </Box>
-      </>
-    );
+            </Container>
+          </Box>
+        </>
+      );
+    }
   }
 }
 
