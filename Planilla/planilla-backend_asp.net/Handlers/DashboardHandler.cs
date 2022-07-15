da20db8fdae5061c1a630bf1b8fef25e4e923279
Emmanuel S <emmanuel.solisp@icloud.com>
2022-07-15 17:56:21 -0600

    added methods for employee types and next payments

--- Planilla/planilla-backend_asp.net/Handlers/DashboardHandler.cs
+++ Planilla/planilla-backend_asp.net/Handlers/DashboardHandler.cs
@@ -18,7 +18,17 @@ namespace planilla_backend_asp.net.Handlers
       conexion = new SqlConnection(rutaConexion);
     }
 
-    public DashboardEmployerModel GetDashboard(string employerID, string projectName)
+    private DataTable CreateTableConsult(SqlDataAdapter tableAdapter)
+    {
+      DataTable consultTable = new DataTable();
+      conexion.Open();
+      tableAdapter.Fill(consultTable);
+      conexion.Close();
+
+      return consultTable;
+    }
+
+    public DashboardEmployerModel GetDashboard(string employerID)
     {
       DashboardEmployerModel dashboard = new DashboardEmployerModel();
       try
@@ -34,43 +44,20 @@ namespace planilla_backend_asp.net.Handlers
         cmd.Parameters.AddWithValue("@employerID", employerID);
         dashboard.totalProjects = cmd.ExecuteScalar().ToString();
 
-        // Total employees hired by this employer by project.
-        cmd = new SqlCommand("select distinct count(EmployeeID) as TotalEmployeesByProject from Contracts where ProjectName=@projectName", conexion);
-        cmd.Parameters.AddWithValue("@projectName", projectName);
-        dashboard.totalEmployeesByProject = cmd.ExecuteScalar().ToString();
+        if (cmd.Connection.State == ConnectionState.Open)
+        {
+          cmd.Connection.Close();
+        }
 
-        // Total cost for benefits.
-        cmd = new SqlCommand("select SUM(Cost) as TotalBenefitsCostByProject from Benefits where ProjectName=@projectName and EmployerID=@employerID", conexion);
-        cmd.Parameters.AddWithValue("@employerID", employerID);
-        cmd.Parameters.AddWithValue("@projectName", projectName);
-        dashboard.costForBenefits = cmd.ExecuteScalar().ToString();
-
-        // Total full time employees.
-        cmd = new SqlCommand("select distinct count(EmployeeID) as FullTimeEmployees from Contracts where ProjectName=@projectName and EmployerID=@employerID and ContractType=0", conexion);
-        cmd.Parameters.AddWithValue("@employerID", employerID);
-        cmd.Parameters.AddWithValue("@projectName", projectName);
-        dashboard.totalFulltimeEmployees = cmd.ExecuteScalar().ToString();
-
-        // Total part time employees.
-        cmd = new SqlCommand("select distinct count(EmployeeID) as PartTimeEmployees from Contracts where ProjectName=@projectName and EmployerID=@employerID and ContractType=1", conexion);
-        cmd.Parameters.AddWithValue("@employerID", employerID);
-        cmd.Parameters.AddWithValue("@projectName", projectName);
-        dashboard.totalPartTimeEmployees = cmd.ExecuteScalar().ToString();
-
-        // Total hourly employees.
-        cmd = new SqlCommand("select distinct count(EmployeeID) as HourlyEmployees from Contracts where ProjectName=@projectName and EmployerID=@employerID and ContractType=2", conexion);
-        cmd.Parameters.AddWithValue("@employerID", employerID);
-        cmd.Parameters.AddWithValue("@projectName", projectName);
-        dashboard.totalHourlyEmployees = cmd.ExecuteScalar().ToString();
-
-        // Total professional services employees.
-        cmd = new SqlCommand("select distinct count(EmployeeID) as ProfessionalServicesEmployees from Contracts where ProjectName=@projectName and EmployerID=@employerID and ContractType=3", conexion);
-        cmd.Parameters.AddWithValue("@employerID", employerID);
-        cmd.Parameters.AddWithValue("@projectName", projectName);
-        dashboard.totalProfessionalServicesEmployees = cmd.ExecuteScalar().ToString();
+        // Employee types by project
+        dashboard.totalEmployeesByProject = GetEmployeeTypesByProject(employerID);
+        
+        // Next Payments
+        dashboard.nextPayments = GetNextPayments(employerID);
       }
       catch (Exception e)
       {
+        Console.WriteLine(e);
         return null;
       }
       finally
@@ -78,6 +65,112 @@ namespace planilla_backend_asp.net.Handlers
         conexion.Close();
       }
       return dashboard;
+    }
+
+    private List<TotalEmployeesByProject> GetEmployeeTypesByProject(string employerID)
+    {
+      List<TotalEmployeesByProject> employeeTypes = new List<TotalEmployeesByProject>();
+      var consult = @"select p.ProjectName, 
+              (select count(c.EmployeeID) from Contracts c where c.EmployerID=@employerID and ContractType=0 and ProjectName = p.ProjectName) as FullTime,
+              (select count(c.EmployeeID) from Contracts c where c.EmployerID=@employerID and ContractType=1 and ProjectName = p.ProjectName) as PartTime,
+              (select count(c.EmployeeID) from Contracts c where c.EmployerID=@employerID and ContractType=2 and ProjectName = p.ProjectName) as Hourly,
+              (select count(c.EmployeeID) from Contracts c where c.EmployerID=@employerID and ContractType=3 and ProjectName = p.ProjectName) as ProfServices
+        from Projects p
+        where EmployerID=@employerID";
+      var queryCommand = new SqlCommand(consult, conexion);
+
+      // Uses user's email and the name of the active project to get only related benefits
+      queryCommand.Parameters.AddWithValue("@employerID", employerID);
+
+      SqlDataAdapter tableAdapter = new SqlDataAdapter(queryCommand);
+      DataTable tablaResultado = CreateTableConsult(tableAdapter);
+      foreach (DataRow columna in tablaResultado.Rows)
+      {
+        employeeTypes.Add(new TotalEmployeesByProject
+        {
+          projectName = Convert.ToString(columna["ProjectName"]),
+          totalFullTime = Convert.ToString(columna["FullTime"]),
+          totalPartTime = Convert.ToString(columna["PartTime"]),
+          totalHourly = Convert.ToString(columna["Hourly"]),
+          totalProfessionalServices = Convert.ToString(columna["ProfServices"])
+        });
+      }
+
+      return employeeTypes;
+    }
+
+    private List<NextPayments> GetNextPayments(string employerID)
+    {
+      List<NextPayments> nextPayments = new List<NextPayments>();
+      var consult = @"select distinct p.ProjectName,
+            p.PaymentMethod,
+            (select distinct paid.PaymentDate from Payments paid where EmployerID=@employerID and paid.ProjectName=p.ProjectName) as LastPayment
+        from Projects p
+        where EmployerID=@employerID";
+      var queryCommand = new SqlCommand(consult, conexion);
+
+      // Uses user's email and the name of the active project to get only related benefits
+      queryCommand.Parameters.AddWithValue("@employerID", employerID);
+
+      SqlDataAdapter tableAdapter = new SqlDataAdapter(queryCommand);
+      DataTable tablaResultado = CreateTableConsult(tableAdapter);
+      foreach (DataRow columna in tablaResultado.Rows)
+      {
+        string nextPaymentDate = Convert.ToString(columna["LastPayment"]);
+          switch (Convert.ToString(columna["PaymentMethod"]))
+          {
+            case "NULL":
+              {
+                nextPaymentDate = "-1";
+                break;
+              }
+            case "Weekly":
+              {
+                nextPaymentDate = AddDaysToDate(nextPaymentDate, 7);
+                break;
+              }
+            case "Biweekly":
+              {
+                nextPaymentDate = AddDaysToDate(nextPaymentDate, 14);
+                break;
+              }
+            case "Monthly":
+              {
+                nextPaymentDate = AddDaysToDate(nextPaymentDate, 30);
+                break;
+              }
+            default:
+              {
+                nextPaymentDate = "-1";
+                break;
+              }
+          }
+        nextPayments.Add(
+          new NextPayments {
+            projectName = Convert.ToString(columna["ProjectName"]),
+            paymentFrequency = Convert.ToString(columna["PaymentMethod"]),
+            nextPayment = nextPaymentDate,
+          }
+        );
+        
+      }
+
+      return nextPayments;
+    }
+
+    private string AddDaysToDate(string date01, int numberDays)
+    {
+      try
+      {
+        DateTime date = Convert.ToDateTime(date01);
+        DateTime newDate = date.AddDays(numberDays);
+        return newDate.ToString("dd-MM-yyyy"); 
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+        return "-1";
+      }
     }
   }
 }
