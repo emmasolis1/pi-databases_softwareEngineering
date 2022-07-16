39172132a4a263102d6e4c2a6e5c7f2cda3c65b6
Kevin <kevinarguedas1820@gmail.com>
2022-07-16 11:40:55 -0600

    Added the first procedure for the employee payment history, the model and basic construction methods for handler

---
+++ Planilla/planilla-backend_asp.net/Handlers/PaymentHistoryHandler.cs
@@ -0,0 +1,29 @@
+﻿using planilla_backend_asp.net.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace planilla_backend_asp.net.Handlers
+{ 
+    public class PaymentHistoryHandler
+    {
+        private static SqlConnection connection;
+        private string connectionRoute;
+        public PaymentHistoryHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            connectionRoute = builder.Configuration.GetConnectionString("EmpleadorContext");
+            connection = new SqlConnection(connectionRoute);
+        }
+        private DataTable CreateTableConsult(SqlCommand queryCommand)
+        {
+            SqlDataAdapter tableAdapter = new SqlDataAdapter(queryCommand);
+            DataTable tableFormatQuery = new DataTable();
+            connection.Open();
+            tableAdapter.Fill(tableFormatQuery);
+            connection.Close();
+            return tableFormatQuery;
+        }
+        
+    }
+
+}
