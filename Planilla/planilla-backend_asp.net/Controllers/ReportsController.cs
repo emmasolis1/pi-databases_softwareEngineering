8d4bf3e53da4d092f30a9abfb0347b55aff7b84c
Emmanuel S <emmanuel.solisp@icloud.com>
2022-07-18 23:51:15 -0600

    Created base controller for reports

---
+++ Planilla/planilla-backend_asp.net/Controllers/ReportsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using planilla_backend_asp.net.Models;
+using planilla_backend_asp.net.Handlers;
+
+namespace planilla_backend_asp.net.Controllers
+{
+  [Route("api/")]
+  [ApiController]
+  public class ReportsController : ControllerBase
+  {
+    // This returns all the 10 latest reports for the given employeeID.
+    [HttpGet]
+    [Route("reports")]
+    public ActionResult GetProjects(string employeeID)
+    {
+      var handler = new ReportsHandler();
+      var data = handler.GetEmployeeReports(employerID);
+      return Ok(data);
+    }
+  }
+}
