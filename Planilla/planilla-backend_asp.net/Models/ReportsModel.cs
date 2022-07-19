31026630684c527f55fece678646d4d62f34f9b1
Emmanuel S <emmanuel.solisp@icloud.com>
2022-07-19 00:14:08 -0600

    Created model for full employee report

--- Planilla/planilla-backend_asp.net/Models/ReportsModel.cs
+++ Planilla/planilla-backend_asp.net/Models/ReportsModel.cs
@@ -6,4 +6,28 @@ namespace planilla_backend_asp.net.Models
     public string employerID { get; set; }
     public string paymentDate { get; set; }
   }
+
+  public class EmployeeReport
+  {
+    public string projectName { get; set; }
+    public string paymentDate { get; set; }
+    public string employeeName { get; set; }
+    public string contractType { get; set; }
+    public string netSalary { get; set; }
+    public string grossSalary { get; set; }
+    public List<MandatoryDeductionsEmployeeReport>? mandatoryDeductions { get; set; } = new List<MandatoryDeductionsEmployeeReport>();
+    public List<VoluntaryDeductionsEmployeeReport>? optionalDeductions { get; set; } = new List<VoluntaryDeductionsEmployeeReport>();
+  }
+
+  public class MandatoryDeductionsEmployeeReport
+  {
+    public string name { get; set; }
+    public string cost { get; set; }
+  }
+
+  public class VoluntaryDeductionsEmployeeReport
+  {
+    public string name { get; set; }
+    public string cost { get; set; }
+  }
 }
