3a59ff23f559053243f84f10cd6079549843f93b
Gabriel Zúñiga Orozco <GABRIEL.ZUNIGAOROZCO@ucr.ac.cr>
2022-07-20 18:59:37 -0600

    Implemented document download

--- Planilla/planilla-frontend_react/src/components/employeePaymentHistoryReport/report-list-results.js
+++ Planilla/planilla-frontend_react/src/components/employeePaymentHistoryReport/report-list-results.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import PropTypes from 'prop-types';
-import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { utils, writeFile } from 'xlsx';
 import {
-  Avatar,
   Box,
+  Button,
   Card,
   Table,
   TableBody,
@@ -13,7 +13,6 @@ import {
   TableHead,
   TablePagination,
   TableRow,
-  Typography
 } from '@mui/material';
 
 export const ReportListResults = ({ reports, ...rest }) => {
@@ -29,103 +28,126 @@ export const ReportListResults = ({ reports, ...rest }) => {
     setPage(newPage);
   };
 
+  const exportToExcel = (sheetName, fileName) => {
+    let workBook = utils.book_new(), workSheet = utils.json_to_sheet(reports);
+    utils.book_append_sheet(workBook, workSheet, sheetName);
+    writeFile(workBook, `${fileName}.xlsx`);
+  }
+
   return (
-    <Card {...rest}>
-      <PerfectScrollbar>
-        <Box sx={{ minWidth: 1050 }}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  Employee's Name
-                </TableCell>
-                <TableCell>
-                  Employee's ID
-                </TableCell>
-                <TableCell>
-                  Project
-                </TableCell>
-                <TableCell>
-                  Contract Type
-                </TableCell>
-                <TableCell>
-                  Payment Date
-                </TableCell>
-                <TableCell>
-                  Gross Salary
-                </TableCell>
-                <TableCell>
-                  Total Benefits Cost
-                </TableCell>
-                <TableCell>
-                  Total Employer Mandatory Deductions
-                </TableCell>
-                <TableCell>
-                  Total employee Mandatory Deductions
-                </TableCell>
-                <TableCell>
-                  Total employee Voluntary Deductions
-                </TableCell>
-                <TableCell>
-                  Total Cost
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {reports.slice(page * limit, page * limit + limit).map(report => (
-                <TableRow
-                  hover
-                  key={report.employeeName + report.employeeID + report.projectName + report.contractType + report.paymentDate}
-                >
+    <>
+      <Card {...rest}>
+        <PerfectScrollbar>
+          <Box sx={{ minWidth: 1050 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
                   <TableCell>
-                    {report.employeeName}
+                    Employee's Name
                   </TableCell>
                   <TableCell>
-                    {report.employeeID}
+                    Employee's ID
                   </TableCell>
                   <TableCell>
-                    {report.projectName}
+                    Project
                   </TableCell>
                   <TableCell>
-                    {report.contractType}
+                    Contract Type
                   </TableCell>
                   <TableCell>
-                    {report.paymentDate}
+                    Payment Date
                   </TableCell>
                   <TableCell>
-                    ${parseFloat(report.grossSalary).toFixed(2)}
+                    Gross Salary
                   </TableCell>
                   <TableCell>
-                    ${parseFloat(report.benefitsCost).toFixed(2)}
+                    Total Benefits Cost
                   </TableCell>
                   <TableCell>
-                    ${parseFloat(report.employerMandatoryDeductions).toFixed(2)}
+                    Total Employer Mandatory Deductions
                   </TableCell>
                   <TableCell>
-                    ${parseFloat(report.employeeMandatoryDeductions).toFixed(2)}
+                    Total employee Mandatory Deductions
                   </TableCell>
                   <TableCell>
-                    ${parseFloat(report.voluntaryDeductions).toFixed(2)}
+                    Total employee Voluntary Deductions
                   </TableCell>
                   <TableCell>
-                    ${parseFloat(report.totalCost).toFixed(2)}
+                    Total Cost
                   </TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Box>
-      </PerfectScrollbar>
-      <TablePagination
-        component="div"
-        count={reports.length}
-        onPageChange={handlePageChange}
-        onRowsPerPageChange={handleLimitChange}
-        page={page}
-        rowsPerPage={limit}
-        rowsPerPageOptions={[5, 10, 25]}
-      />
-    </Card>
+              </TableHead>
+              <TableBody>
+                {reports.slice(page * limit, page * limit + limit).map(report => (
+                  <TableRow
+                    hover
+                    key={report.employeeName + report.employeeID + report.projectName + report.contractType + report.paymentDate}
+                  >
+                    <TableCell>
+                      {report.employeeName}
+                    </TableCell>
+                    <TableCell>
+                      {report.employeeID}
+                    </TableCell>
+                    <TableCell>
+                      {report.projectName}
+                    </TableCell>
+                    <TableCell>
+                      {report.contractType}
+                    </TableCell>
+                    <TableCell>
+                      {report.paymentDate}
+                    </TableCell>
+                    <TableCell>
+                      ${parseFloat(report.grossSalary).toFixed(2)}
+                    </TableCell>
+                    <TableCell>
+                      ${parseFloat(report.benefitsCost).toFixed(2)}
+                    </TableCell>
+                    <TableCell>
+                      ${parseFloat(report.employerMandatoryDeductions).toFixed(2)}
+                    </TableCell>
+                    <TableCell>
+                      ${parseFloat(report.employeeMandatoryDeductions).toFixed(2)}
+                    </TableCell>
+                    <TableCell>
+                      ${parseFloat(report.voluntaryDeductions).toFixed(2)}
+                    </TableCell>
+                    <TableCell>
+                      ${parseFloat(report.totalCost).toFixed(2)}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Box>
+        </PerfectScrollbar>
+        <TablePagination
+          component="div"
+          count={reports.length}
+          onPageChange={handlePageChange}
+          onRowsPerPageChange={handleLimitChange}
+          page={page}
+          rowsPerPage={limit}
+          rowsPerPageOptions={[5, 10, 25]}
+        />
+      </Card>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          p: 2
+        }}
+      >
+        <Button
+          color="secondary"
+          variant="contained"
+          onClick={() => exportToExcel("Employee Payment History", "Employee_Payment_History")}
+        >
+          Download document
+        </Button>
+      </Box>
+    </>
   );
 };
 
